4559c79c5680a5a52b6ddff4290154af819b3655
ronanhwang24 <ronan.hwang@gmail.com>
2025-04-21 20:42:09 -0400

    Update app2.js

--- app2.js
+++ app2.js
@@ -64,7 +64,7 @@ app.get("/process", async (req, res) => {
 });
 
 // Port Configuration
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
